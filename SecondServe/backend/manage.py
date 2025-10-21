4c159f4e462dbbb2a5f677d67f54b8906610e7ce
lil_cosine <baeden.vester@gmail.com>
2025-10-20 20:49:45 -0400

    python hook test

--- SecondServe/backend/manage.py
+++ SecondServe/backend/manage.py
@@ -11,6 +11,7 @@ def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SecondServe.settings')
     try:
         from django.core.management import execute_from_command_line
+        print("test")
     except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
