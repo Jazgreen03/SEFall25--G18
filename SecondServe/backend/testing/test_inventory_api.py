ca166f5c692f6990a1a4518d5ce94ce9f54f3da1
Caleb Twigg <calebtwigg248@gmail.com>
2025-10-31 10:57:27 -0400

    Continuing with Inventory API test case creation

--- SecondServe/backend/testing/test_inventory_api.py
+++ SecondServe/backend/testing/test_inventory_api.py
@@ -44,9 +44,34 @@ class TestInvalidUser(TestCase):
         response = self.client.get("/inventory/")
         self.assertEqual(response.status_code, 404)
 
-    # Try getting an inventory without a logged in user
+        # Try to add an item
+        response = self.client.post("/inventory/add/")
+        self.assertEqual(response.status_code, 404)
+
+        # Try to update the inventory
+        response = self.client.put("/inventory/update/")
+        self.assertEqual(response.status_code, 404)
+
+        # Try to edit an item
+        response = self.client.put("/inventory/edit/")
+        self.assertEqual(response.status_code, 404)
+
+    # No Logged in User
     def test_no_logged_in_user(self):
+        # Try to get the inventory
         response = self.client.get("/inventory/")
+        self.assertEqual(response.status_code, 400)
+
+        # Try to add an item
+        response = self.client.post("/inventory/add/")
+        self.assertEqual(response.status_code, 400)
+
+        # Try to update the inventory
+        response = self.client.put("/inventory/update/")
+        self.assertEqual(response.status_code, 400)
+
+        # Try to edit an item
+        response = self.client.put("/inventory/edit/")
         self.assertEqual(response.status_code, 400)
 
     # Creates a user with the user permission and tries to get an inventory
@@ -73,9 +98,22 @@ class TestInvalidUser(TestCase):
         response = self.client.get("/inventory/")
         self.assertEqual(response.status_code, 401)
 
+        # Try to add an item
+        response = self.client.post("/inventory/add/")
+        self.assertEqual(response.status_code, 401)
+
+        # Try to update the inventory
+        response = self.client.put("/inventory/update/")
+        self.assertEqual(response.status_code, 401)
+
+        # Try to edit an item
+        response = self.client.put("/inventory/edit/")
+        self.assertEqual(response.status_code, 401)
+
 class TestAddItem(TestCase):
 
     # Creates a user with the location permission for testing purposes
+    # User is also associated with an organization
     def setUp(self):
         self.user = User.objects.create(
             username="orgUser",
@@ -84,10 +122,29 @@ class TestAddItem(TestCase):
         )
         self.user.role = "location"
         self.user.save()
+
+        self.client.post(
+            "/user/login/",
+            {
+                "email": "org@email.com",
+                "password": "123abc"
+            }
+        )
+
+        self.client.post(
+            "org/", 
+            {
+                "name": orgName,
+                "type": valid_orgType,
+                "location": orgLocation
+            }
+        )
+
 
 class TestUpdateInventory(TestCase):
 
     # Creates a user with the location permission for testing purposes
+    # User is also associated with an organization
     def setUp(self):
         self.user = User.objects.create(
             username="orgUser",
@@ -96,10 +153,28 @@ class TestUpdateInventory(TestCase):
         )
         self.user.role = "location"
         self.user.save()
+
+        self.client.post(
+            "/user/login/",
+            {
+                "email": "org@email.com",
+                "password": "123abc"
+            }
+        )
+
+        self.client.post(
+            "org/", 
+            {
+                "name": orgName,
+                "type": valid_orgType,
+                "location": orgLocation
+            }
+        )
 
 class TestEditItem(TestCase):
 
     # Creates a user with the location permission for testing purposes
+    # User is also associated with an organization
     def setUp(self):
         self.user = User.objects.create(
             username="orgUser",
@@ -109,9 +184,27 @@ class TestEditItem(TestCase):
         self.user.role = "location"
         self.user.save()
 
+        self.client.post(
+            "/user/login/",
+            {
+                "email": "org@email.com",
+                "password": "123abc"
+            }
+        )
+
+        self.client.post(
+            "org/", 
+            {
+                "name": orgName,
+                "type": valid_orgType,
+                "location": orgLocation
+            }
+        )
+
 class TestGetInventory(TestCase):
     
     # Creates a user with the location permission for testing purposes
+    # User is also associated with an organization
     def setUp(self):
         self.user = User.objects.create(
             username="orgUser",
@@ -120,3 +213,20 @@ class TestGetInventory(TestCase):
         )
         self.user.role = "location"
         self.user.save()
+
+        self.client.post(
+            "/user/login/",
+            {
+                "email": "org@email.com",
+                "password": "123abc"
+            }
+        )
+
+        self.client.post(
+            "org/", 
+            {
+                "name": orgName,
+                "type": valid_orgType,
+                "location": orgLocation
+            }
+        )
