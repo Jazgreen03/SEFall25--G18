c0284aad850640f68fd13cfdbd5930052a6e1009
Caleb Twigg <calebtwigg248@gmail.com>
2025-10-31 09:14:54 -0400

    Finished Org API test cases

--- SecondServe/backend/testing/test_organization_api.py
+++ SecondServe/backend/testing/test_organization_api.py
@@ -31,7 +31,7 @@ class TestValidUserCreateOrganization(TestCase):
         self.user.save()
 
         self.client.post(
-            "/user/login",
+            "/user/login/",
             {
                 "email": "org@email.com",
                 "password": "123abc"
@@ -50,11 +50,98 @@ class TestValidUserCreateOrganization(TestCase):
             }
         )
 
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, 201)
 
     # Attempt to create Invalid Organization and verify the response
+    def test_invalid_org_create(self):
+        
+        # Create the base case valid organization
+        self.client.post(
+            "org/", 
+            {
+                "name": orgName,
+                "type": valid_orgType,
+                "location": orgLocation
+            }
+        )
 
-    # Attempt to create Organization with user account with invalid credentials
 
-    
+        # Invalid Type
+        response = self.client.post(
+            "org/", 
+            {
+                "name": orgName,
+                "type": invalid_orgType,
+                "location": orgLocation
+            }
+        )
 
+        self.assertEqual(response.status_code, 404)
+
+        # Invalid Name
+        response = self.client.post(
+            "org/", 
+            {
+                "name": orgName,
+                "type": valid_orgType,
+                "location": orgLocationTwo
+            }
+        )
+
+        self.assertEqual(response.status_code, 409)
+
+        # Invalid Location
+        response = self.client.post(
+            "org/", 
+            {
+                "name": orgNameTwo,
+                "type": valid_orgType,
+                "location": orgLocation
+            }
+        )
+
+        self.assertEqual(response.status_code, 409)
+
+
+# Attempt to create Organization with user account with invalid credentials
+class TestInvalidUserCreateOrganization(TestCase):
+
+    def test_no_logged_in_user(self):
+        response = self.client.post(
+            "org/", 
+            {
+                "name": orgName,
+                "type": valid_orgType,
+                "location": orgLocation
+            }
+        )
+
+        self.assertEqual(response.status_code, 400)
+
+    def test_invalid_role(self):
+        self.user = User.objects.create(
+            username="orgUser",
+            email="org@email.com",
+            password="123abc"
+        )
+        self.user.role = "user"
+        self.user.save()
+
+        self.client.post(
+            "/user/login/",
+            {
+                "email": "org@email.com",
+                "password": "123abc"
+            }
+        )
+
+        response = self.client.post(
+            "org/", 
+            {
+                "name": orgName,
+                "type": valid_orgType,
+                "location": orgLocation
+            }
+        )
+
+        self.assertEqual(response.status_code, 401)
